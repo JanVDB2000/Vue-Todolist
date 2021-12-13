7eb284114171212ab01e46c478dba7408cf77d1f
JanVDB2000 <janvandenbogaert2000@gmail.com>
2021-12-13 14:29:43 +0100

    tweaks

--- componenten/taak-toevoegen.js
+++ componenten/taak-toevoegen.js
@@ -1,7 +1,12 @@
 app.component('taak-toevoegen',{
+    props:{
+        aantal: {
+            type:Array,
+            required:true,
+        }
+    },
     template:
-        `<div class="container-fluid">
-           <div class=" col-10 offset-1">
+        `<div class=" col-10 offset-1">
                  <div class="shadow-lg m-4 mx-auto ">
                     <h1 class="text-center p-3">Takenlijst</h1>
                  </div>
@@ -9,10 +14,11 @@ app.component('taak-toevoegen',{
             <form class=" input-group mb-3 "@submit.prevent="onSubmit">
                 <input type="text" class="form-control" placeholder="Taak..." aria-label="Recipient's username" aria-describedby="button-addon2" id="taak" v-model="taak">
                 <button @click="toevoegenEenTaak" class="btn btn-outline-secondary" type="submit"  id="button-addon2" for="toDotask"><i class="bi bi-plus-square-fill"></i></button>
+                <button @click="delAll" class="btn btn-outline-danger" type="button">Delete All</button>
+                <span class="input-group-text">Aantal : {{aantal.length}} </span>
             </form>
              </div>
-           </div>
-         </div>`,
+           </div>`,
     data(){
         return{
             taak:'',
@@ -25,6 +31,9 @@ app.component('taak-toevoegen',{
             }
             this.$emit('toevoegenEenTaak',  eenTaakToevoegen)
             this.taak= '';
+        },
+        delAll(){
+            this.$emit('delAll')
         }
-    },
+    }
 })
--- componenten/taken-formulier.js
+++ componenten/taken-formulier.js
@@ -1,15 +1,15 @@
 app.component('taken-formulier', {
-    template: `<taak-toevoegen @toevoegenEenTaak="eenTaakToevoegen"  ></taak-toevoegen>
+    template: `<taak-toevoegen @toevoegenEenTaak="eenTaakToevoegen" @delAll="verwijderlijst" :aantal="checkboxlijst"></taak-toevoegen>
                 <hr>
                 <div class="col-lg-6 offset-3 mx-auto">
-                <div v-for="(taak,index) in taken" :key="index" class="input-group mb-3">
+                <div v-for="(taak,index) in taken" :key="index" class="input-group mb-3 w-100">
                 <div>
                 <div class="input-group mb-3">
                   
                     <div class="input-group-text">
-                       <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
+                       <input @click="checkb(index)" class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
                     </div>
-                        <input v-model="taak.taak" type="text" class="form-control" aria-label="Text input with checkbox">
+                        <input v-model="taak.taak" type="text" class="form-control" :class="{ 'text-decoration-line-through fw-bold': checkboxlijst[index] }" aria-label="Text input with checkbox">
                         <button @click="verwijderden" class="btn btn-outline-secondary" type="submit"  id="button-addon2" ><i class="bi bi-trash-fill"></i></button>
                 </div>
                 </div>
@@ -18,13 +18,29 @@ app.component('taken-formulier', {
     , data() {
         return {
             taken: [],
+            checkboxlijst: [],
+            isActive: false,
         }
     }, methods: {
         eenTaakToevoegen(taak) {
             this.taken.push(taak);
+            this.checkboxlijst.push(this.isActive );
         },
-        verwijderden(){
-            this.taken.slice(1)
+        verwijderden(index){
+            this.taken.splice(index,1)
+            this.checkboxlijst.splice(index,1)
+        },
+        verwijderlijst(){
+            this.taken = [];
+            this.checkboxlijst = [];
+        },
+        checkb(index){
+            if (this.checkboxlijst[index] === false){
+                this.checkboxlijst[index] = true
+            } else{
+                this.checkboxlijst[index] = false
+            }
+
         }
     }
 })
